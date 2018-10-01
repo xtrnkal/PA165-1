7a45dd6ca6a6f0738637ba602321b20cba202e27
Anna Trnkalová <xtrnkal@fi.muni.cz>
2018-10-01 20:09:06 +0200

    Implemeted function convert()

--- currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
@@ -1,7 +1,11 @@
 package cz.muni.fi.pa165.currency;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Currency;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -20,7 +24,27 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     @Override
     public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (sourceCurrency == null) {
+            throw new IllegalArgumentException("Source currency can't be null.");
+        } else if (targetCurrency == null) {
+            throw new IllegalArgumentException("Target currency can't be null.");
+        } else if (sourceAmount == null) {
+            throw new IllegalArgumentException("Source amount can't be null.");
+        }
+        
+        BigDecimal result;
+        
+        try {
+            result = exchangeRateTable.getExchangeRate(sourceCurrency, targetCurrency);
+            
+        } catch (UnknownExchangeRateException ex1) {
+            Logger.getLogger(CurrencyConvertorImpl.class.getName()).log(Level.SEVERE, null, ex1);
+            throw new UnknownExchangeRateException("Given currency is unknown.");
+        } catch (ExternalServiceFailureException ex2) {
+            throw new UnknownExchangeRateException("Lookup for exchange rate failed.", ex2);
+        }
+        
+        return result.multiply(sourceAmount).setScale(2, RoundingMode.HALF_EVEN);
     }
 
 }
